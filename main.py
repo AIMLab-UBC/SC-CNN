1a3c98927d2047734bb59c784faad62b2b526034
Amirali <65702018+Amiiirali@users.noreply.github.com>
2020-06-14 19:58:31 -0700

    Comment Fix

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 '''
 This is the implementation of following paper:
 
-    @inproceedings{Vahadane2015ISBI,
+    @inproceedings{
     Author = {Korsuk Sirinukunwattana and Shan E Ahmed Raza and Yee-Wah Tsang and David R. J. Snead and Ian A. Cree and Nasir M. Rajpoot},
     Booktitle = {IEEE TRANSACTIONS ON MEDICAL IMAGING},
     Title = {Locality Sensitive Deep Learning for Detection and Classification of Nuclei in Routine Colon Cancer Histology Images},
