c55e023246a6247b84448505724cb2468e4e1be8
Amirali <65702018+Amiiirali@users.noreply.github.com>
2020-06-21 17:07:10 -0700

    Update
    Adding backpropagation of 3 versions.

--- model/HeatMap.py
+++ model/HeatMap.py
@@ -14,13 +14,21 @@ class HeatMap(torch.autograd.Function):
     '''
 
     @staticmethod
-    def forward(ctx, points, h_value, device, d, out_size):
+    def forward(ctx, points, h_value, device, d, out_size, version):
 
-        heat_map = utils.heat_map_tensor(points, h_value, device, d, out_size)
+        if version==2:
+            heat_map = utils.heat_map_tensor(points.view(-1, 2),
+                                             h_value.view(-1, 1) ,device, d,
+                                             out_size)
+        if version==1 or version==0:
+            heat_map = utils.heat_map_tensor(points, h_value ,device, d,
+                                             out_size)
+
         ctx.save_for_backward(points, h_value, heat_map)
 
         ctx.out_size = out_size
         ctx.device = device
+        ctx.version = version
 
         return heat_map
 
@@ -28,17 +36,22 @@ class HeatMap(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
 
-        out_size, device = ctx.out_size, ctx.device
+        out_size, device, version = ctx.out_size, ctx.device, ctx.version
         points, h_value, heat_map = ctx.saved_tensors
+
+        if version==2:
+             points  = points.view(-1, 2)
+             h_value = h_value.view(-1, 1)
 
         batch_size = heat_map.shape[0]
         H, W = out_size[0], out_size[1]
+
         temp = torch.pow(heat_map, 2) / h_value.view(batch_size, 1, 1)
-        
         # replace Nan with zero
         temp[temp != temp] = 0
-        
-        coords = torch.tensor([[h, w] for h in range(H) for w in range(W)], device=device)
+
+        coords = torch.tensor([[h, w] for h in range(H) for w in range(W)],
+                              device=device)
 
         x = coords[:, 0]; y = coords[:, 1]
         x, y = x.reshape(H, W), y.reshape(H, W)
@@ -59,5 +72,8 @@ class HeatMap(torch.autograd.Function):
         grad_h_value = heat_map / h_value.view(heat_map.shape[0], 1, 1)
         grad_h_value = torch.sum(grad_h_value*grad_output, (1,2)).reshape(batch_size, 1)
 
+        if version==2:
+            grad_points = grad_points.view(-1, 2, 1, 1)
+            grad_h_value = grad_h_value.view(-1, 1, 1, 1)
 
-        return grad_points, grad_h_value, None, None, None
+        return grad_points, grad_h_value, None, None, None, None
